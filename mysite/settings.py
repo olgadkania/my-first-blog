aaee3db50d119fed7cfcdf9ac0b0fdf822bfd506
olgaarch <olgadkania@gmail.com>
2016-11-19 15:41:24 +0100

    olgaarch.pythonanywhere.com

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n$grcz@5w(*m@p=ww1+=u+y63wz6%+349o7nxdadk%1#+q&5fo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["olgaarch.pythonanywhere.com"]
 
 
 # Application definition
